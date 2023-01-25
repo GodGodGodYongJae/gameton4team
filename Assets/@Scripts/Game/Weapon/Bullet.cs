32cab79c233a5939b30f94590729497bcf7b6a6f
meleegod <wotjd0963@naver.com>
2023-01-25 22:38:10 +0900

    fix

--- Assets/@Scripts/Game/Weapon/Bullet.cs
+++ Assets/@Scripts/Game/Weapon/Bullet.cs
@@ -14,12 +14,10 @@ namespace Assets._Scripts.Game.Weapon
         private Player player;
         private bool isInit = false;
         private BoxCollider2D box;
-        Weapon_Wand_n wand;
-        public float speed = 10;
+
 
         private void Awake()
         {
-            wand = GameObject.Find("L_Weapon").GetComponent<Weapon_Wand_n>();
             // 자식한테 보통 이펙트가 있음 만약 해당 이펙트에 자식이 없거나 이상하다면
             // 이거 Bullet 자체를 부모 클래스로 빼서, Child를 일일히 지정해줘야할듯..?
             BoxCollider2D childBox = this.gameObject.transform.GetChild(0)
@@ -29,12 +27,7 @@ namespace Assets._Scripts.Game.Weapon
             this.box.size = childBox.size;
             this.box.isTrigger = true;
         }
-        void Update()
-        {
-            float moveX = speed * Time.deltaTime;
-            Debug.Log(wand.direction);
-            transform.Translate(wand.direction * moveX, 0, 0);
-        }
+
         public void InitBulletData(WeaponData weaponData, Player player)
         {
             damagedMonsterList.Clear();
