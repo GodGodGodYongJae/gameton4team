6655c1b7eb1950bf24af69688c0409a6ecf9b986
meleegod <wotjd0963@naver.com>
2023-02-08 21:48:23 +0900

    stage

--- Assets/@Scripts/UI/CurrentStage.cs
+++ Assets/@Scripts/UI/CurrentStage.cs
@@ -8,11 +8,11 @@ public class CurrentStage : MonoBehaviour
     Text text;
     public static int currentStage = 1;
     public static Action stage;
-
     private void Start()
     {
         text = GetComponent<Text>();
         stage = () => { GetStage(); };
+
     }
 
     public void GetStage()
@@ -20,6 +20,7 @@ public class CurrentStage : MonoBehaviour
         currentStage += 1;
         text.text =  "Stage " + currentStage.ToString();
         StageBgmChange();
+        StageBackGroundChange();
     }
     
     public void StageBgmChange()
@@ -32,9 +33,21 @@ public class CurrentStage : MonoBehaviour
 
         if (currentStage == 7)
         {
+            
             Managers.Sound.StopBGM();
             Managers.Sound.PlayBGM("InGame3");
         }
     }
 
+    public void StageBackGroundChange()
+    {
+        if(currentStage == 2) 
+            BackGroundChangeController.stage();
+        else if (currentStage == 3)
+            BackGroundChangeController.stage();
+        else if(currentStage == 5)
+            BackGroundChangeController.stage();
+        else if(currentStage == 7)
+            BackGroundChangeController.stage();
+    }
 }
